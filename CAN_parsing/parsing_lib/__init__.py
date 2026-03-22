6bde5b4351009ca19a40f17a382f15e75299ea0d
tylerharvey <tyler.r.harvey@gmail.com>
2026-03-22 11:29:06 -0700

    organizing the parsing notebook

---
+++ CAN_parsing/parsing_lib/__init__.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+import numpy as np
+
+class frame_and_muid(object):
+    def __init__(self, frame_id, muid):
+        self.frame_id = frame_id
+        self.muid = muid
+
+    def __eq__(self, other):
+        return (self.frame_id == other.frame_id) and (self.muid == other.muid)
+
+    def __lt__(self, other):
+        if((self.frame_id <= other.frame_id) and (self.muid < other.muid) or 
+           (self.frame_id < other.frame_id) and (self.muid <= other.muid)):
+            return True
+        else:
+            return False
+    def __gt__(self, other):
+        if((self.frame_id >= other.frame_id) and (self.muid > other.muid) or 
+           (self.frame_id > other.frame_id) and (self.muid >= other.muid)):
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        return f"Frame ID: {self.frame_id}; MUID: {self.muid}"
+
+def populate_dict(dict_name, filename):
+    dict_name['filename'] = filename
+    dict_name['messages'] = np.loadtxt(filename,delimiter=',',dtype=int,skiprows=1, usecols=(6,7,8,9,10,11,12,13),converters=lambda x: int(x,16))
+    dict_name['timestamps'] = np.loadtxt(filename,delimiter=',',dtype=int,skiprows=1, usecols=(0))
+    dict_name['ids'] = np.loadtxt(filename,delimiter=',',skiprows=1, dtype=int,usecols=(1),converters=lambda x : int(x,16))
+    dict_name['hex_ids'] = np.loadtxt(filename,delimiter=',',dtype=str,skiprows=1, usecols=(1))
+    dict_name['hex_messages'] = np.loadtxt(filename,delimiter=',',dtype=str,skiprows=1, usecols=(6,7,8,9,10,11,12,13))
+
+def calculate_unique_message_id(dict_name):
+    assert dict_name['messages'].shape[1] == 8
+    dict_name['messages_unique_ids'] = np.zeros((dict_name['messages'].shape[0],),dtype=int)
+    for i in range(8):
+        dict_name['messages_unique_ids'] += dict_name['messages'][:,i]*16**(16-2*(i+1))
+
+def return_all_messages_for_frame(dict_name, frame_id,return_hex=False):
+    if(return_hex):
+        return dict_name['hex_messages'][np.argwhere(dict_name['ids'] == frame_id)[:,0]]
+    else:
+        return dict_name['messages'][np.argwhere(dict_name['ids'] == frame_id)[:,0]]
+
+def return_all_frames_messages_for_muid_and_frame(dict_name, frame_id, muid,return_hex=False):
+    indices = np.argwhere((dict_name['messages_unique_ids'] == muid)*(dict_name['ids'] == frame_id))[:,0]
+    for index in indices:
+        if(return_hex):
+            return frame_and_muid(dict_name['hex_ids'][index],
+                    dict_name['hex_messages'][index])
+        else:
+            return frame_and_muid(dict_name['ids'][index],
+                    dict_name['messages'][index])
+
+def return_frame_IDs_with_message_changes(dict_name):
+    frame_ids = []
+    for frame_id in np.unique(dict_name['ids']):
+        unique_muids = np.unique(dict_name['messages_unique_ids'][np.argwhere(dict_name['ids'] == frame_id)[:,0]])
+        if(len(unique_muids) > 1):
+            frame_ids.append([frame_id,len(unique_muids)])
+    return frame_ids
+
+def return_frame_IDs_muids(dict_name, reject_muids = []):
+    '''
+    Return a list of all frame IDs and muids for a log.
+    '''
+    frames_and_muids = []
+    for frame_id in np.unique(dict_name['ids']):
+        unique_muids = np.unique(dict_name['messages_unique_ids'][np.argwhere(dict_name['ids'] == frame_id)[:,0]])
+        for unique_muid in unique_muids:
+            if(unique_muid not in reject_muids):
+                indices = np.argwhere((dict_name['messages_unique_ids'] == unique_muid)*(dict_name['ids'] == frame_id))[:,0]
+                frames_and_muids.append(frame_and_muid(frame_id,unique_muid))
+    return frames_and_muids
+
+def return_frame_IDs_muids_with_message_changes_in_timestamp_range(dict_name,timestamp_range, reject_muids = [], threshold = 10):
+    '''
+    This is a search for control signals, rather than physical sensors. Physical sensors can have a nearly infinite number of values.
+    '''
+    frames_and_muids = []
+    for frame_id in np.unique(dict_name['ids']):
+        unique_muids = np.unique(dict_name['messages_unique_ids'][np.argwhere(dict_name['ids'] == frame_id)[:,0]])
+        if(len(unique_muids) < threshold):
+            for unique_muid in unique_muids:
+                if(unique_muid not in reject_muids):
+                    indices = np.argwhere((dict_name['messages_unique_ids'] == unique_muid)*(dict_name['ids'] == frame_id))[:,0]
+                    timestamps = dict_name['timestamps'][indices]
+                    if(timestamps[0] > timestamp_range[0] and timestamps[-1] < timestamp_range[-1]):
+                        frames_and_muids.append(frame_and_muid(frame_id,unique_muid))
+    return frames_and_muids
+
+def return_interesting_timestamps(dict_name,frame_ids):
+    timestamps = []
+    for frame_id in frame_ids:
+        indices = np.argwhere(dict_name['ids'] == frame_id)[:,0]
+        if(len(indices)):
+            max_diff = np.argmax(np.abs(np.diff(dict_name['messages_unique_ids'][indices])))
+            timestamps.append(dict_name['timestamps'][indices][max_diff])
+    return timestamps
+
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ At the moment, this repository contains:
 ## 1. minimal working CAN messages
 two logs (in SavvyCAN format, with timestamps in microseconds) of CAN messages filtered/edited down from logs recorded using SavvyCAN that can be sent back to Ioniq 5s with a battery PTC heater equipped and battery preconditioning mode enabled to [initiate](preconditioning_messages/MWE_preconditioning.csv) or [cancel](preconditioning_messages/MWE_cancel_preconditioning.csv) preconditioning manually. These messages were reverse-engineered by [dragz](https://github.com/dragz) and I. See dragz's [articles](https://github.com/dragz/explorationsincarhacking/tree/main/articles) or our [Ioniqforum notes](https://www.ioniqforum.com/posts/666540/) for more documentation. 
 ## 2. best CAN message logs and parsing script used to generate the MWEs
-I am including two real recorded logs [1](CAN_logs/M-CAN_driving_with_nav_preconditioning_at_end_cleaned.csv) and [2](CAN_logs/M-CAN_start_nav_to_EA_parked_in_D_preconditioning_cleaned.csv) that included activation of preconditioning; two real recorded logs designed as control experments using the built-in navigation but not navigating to a nearby charger [1](CAN_logs/M-CAN_driving_with_nav_to_school_no_preconditioning_including_reroute.csv) and [2](CAN_logs/M-CAN_start_nav_to_school_parked_in_D.csv); and a parsing [script](tools/parsing_MWE.ipynb) that I retroactively edited to show the most valuable parsing steps I took to identify the minimal working examples (MWEs)
+I am including two real recorded logs [1](CAN_logs/M-CAN_driving_with_nav_preconditioning_at_end_cleaned.csv) and [2](CAN_logs/M-CAN_start_nav_to_EA_parked_in_D_preconditioning_cleaned.csv) that included activation of preconditioning; two real recorded logs designed as control experments using the built-in navigation but not navigating to a nearby charger [1](CAN_logs/M-CAN_driving_with_nav_to_school_no_preconditioning_including_reroute.csv) and [2](CAN_logs/M-CAN_start_nav_to_school_parked_in_D.csv); and a parsing [script](CAN_parsing/parsing_MWE.ipynb) that I retroactively edited to show the most valuable parsing steps I took to identify the minimal working examples (MWEs)
 
 ## 3. harness designs
 I have drawings for harness designs including CAN lines:
@@ -14,13 +14,13 @@ I have drawings for harness designs including CAN lines:
 ![drawing 001 with shielded twisted pairs](wiring_harness/M-CAN_dongle_003.pdf)
 The latter drawing explicitly includes twisted pairs for the CAN lines, which may be important for the wire lengths used here. 
 
-I am also including the source files [1](./M-CAN_dongle_001.yml) and [2](./M-CAN_dongle_003.yml) used to render the drawings with [wireviz](https://github.com/wireviz/WireViz/).
+I am also including the source files [1](wiring_harness/M-CAN_dongle_001.yml) and [2](wiring_harness/M-CAN_dongle_003.yml) used to render the drawings with [wireviz](https://github.com/wireviz/WireViz/).
 
 # Tips
 One or two good logs is far more valuable than 10 questionable logs. I had much better success after identifying my best logs and cleaning them (e.g. out-of-range timestamps from extra acquisitions). Think of log acqusition as a scientific experiment: you want a test and a control condition. In the case of preconditioning, that meant setting the nav to a charger nearby vs. to a school nearby. You can also tag logs with known messages. I plan to use the star buttons for this purpose.
 
 # Parsing
-One approach to [parsing](tools/parsing_MWE.ipynb) depends at least in part on interesting timestamp tagging to limit the noise. In our case, this was made possible by dragz's manual discoveries of preconditioning indicator messages.
+One approach to [parsing](CAN_parsing/parsing_MWE.ipynb) depends at least in part on interesting timestamp tagging to limit the noise. In our case, this was made possible by dragz's manual discoveries of preconditioning indicator messages.
 
 
 # More coming soon
